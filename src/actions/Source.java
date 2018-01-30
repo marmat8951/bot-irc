d8c8301205119a1979ac7c496859cd742d217d53
Martin Passard <martin.passard@etudiant.univ-lille1.fr>
2018-01-30 22:19:57 +0100

    feat: Ajout action Source

---
+++ src/actions/Source.java
@@ -0,0 +1,31 @@
+package actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import data.CoveredAreas;
+import data.ISP;
+import data.ISPDAO;
+import main.Bot;
+import main.Cache;
+import verif_saisie.EntierPositifNonVide;
+
+public class Source extends Action {
+
+	public Source(Bot b) {
+		this.bot = b;
+		List<String> ar = new ArrayList<>();
+		ar.add("source");
+		ar.add("sources");
+		ar.add("code");
+		this.keyWords = ar;
+	}
+
+	@Override
+	public void react(String channel, String sender, String login, String hostname, String message) {
+		bot.sendMessage(channel, sender+": mes sources sont disponibles ici: https://code.ffdn.org/marmat8951/bot-irc2");
+	}
+
+}
+
+

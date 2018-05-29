01cfde57aa780a3a1c099a262ebd23ccb60150bb
Martin Passard <git@marmat.ovh>
2018-05-29 18:57:06 -0400

    Update scipt

--- updateCompileAndRun.sh
+++ updateCompileAndRun.sh
@@ -1,1 +1,3 @@
-git pull &&  javac -cp json-20171018.jar:pircbot-1.5.0/pircbot.jar:src:ressources:. src/main/Main.java && java -cp json-20171018.jar:pircbot-1.5.0/pircbot.jar:src:ressources: main.Main
+#!/bin/bash
+find . -type f -name '*.class' -delete;
+git pull &&  javac -cp json-20171018.jar:pircbot-1.5.0/pircbot.jar:commons-io-2.6/commons-io-2.6.jar:src:ressources:. src/main/Main.java && java -cp json-20171018.jar:pircbot-1.5.0/pircbot.jar:commons-io-2.6/commons-io-2.6.jar:src:ressources: main.Main
